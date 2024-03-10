9924dd15f9eca63f9a2558759431e9bac24b4f62
pascalecodes <pdelauna@gmail.com>
2024-03-09 22:32:05 -0800

    add js file for react

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import { createRoot } from 'react-dom/client';
 
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
+function NavigationBar() {
+  // TODO: Actually implement a navigation bar
+  return <h1>Hello from React! change</h1>;
+}
 
-// Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+const domNode = document.getElementById('navigation');
+const root = createRoot(domNode);
+root.render(<NavigationBar />);
