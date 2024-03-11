3d07d4dee0b0fcd580e1587ac6322f3504d1d983
pascalecodes <pdelauna@gmail.com>
2024-03-10 21:57:45 -0700

    edit src file

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,26 @@
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 
-function NavigationBar() {
-  // TODO: Actually implement a navigation bar
-  return <h1>Hello from React! change</h1>;
+// function NavigationBar() {
+//   // TODO: Actually implement a navigation bar
+//   return <h1>Hello from React! change</h1>;
+// }
+
+// const domNode = document.getElementById('navigation');
+// const root = createRoot(domNode);
+// root.render(<NavigationBar />);
+
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Video Recording App</h1>
+
+      <button id="recordButton">Start Recording</button>
+      <p id="prompt"></p>
+
+      <script src="/js/main.js"></script>
+    </div>
+  );
 }
-
-const domNode = document.getElementById('navigation');
-const root = createRoot(domNode);
-root.render(<NavigationBar />);
+export default App;
